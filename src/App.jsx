348e3be3266060b1648bcb1a0b9eddac46630fb4
Loop1397 <shwlsdn0906@gmail.com>
2024-05-27 15:23:51 +0900

    📦 chore : 라우터 추가(깃 클론)
    페이지가 늘어남에 따라 라우터가 추가됨

--- src/App.jsx
+++ src/App.jsx
@@ -1,100 +1,18 @@
-import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-
-import TodoBoard from "./components/TodoBoard";
-
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Container from "react-bootstrap/Container";
-import { useEffect, useState } from "react";
-import api from "./utils/api";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import TodoPage from "./pages/TodoPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
-  // useState안에 들어가 있는 값은 todoList의 초기값으로 사용됨
-  const [todoList, setTodoList] = useState([]);
-  const [todoValue, setTodoValue] = useState('');
-
-  
-
-  const getTasks = async () => {
-    const response = await api.get('/tasks');
-    setTodoList(response.data.data);
-  };
-
-  const addTask = async () => {
-    try {
-      // api 요청. 뒤의 {}는 body로 전달됨
-      const response = await api.post('/tasks', {
-        task:todoValue, 
-        isComplete:false
-      });
-      
-      if(response.status === 200) {
-        console.log('success!');
-        setTodoValue('');
-        getTasks();
-      } else {
-        throw new Error('task can not be added');
-      }
-    } catch (err) {
-      console.log("error", err);
-    }
-  }
-
-  const updateTask = async (id) => {
-    try {
-      const response = await api.put(`/tasks/${id}`);
-      
-      if(response.status === 200) {
-        console.log('success!');
-        getTasks();
-      } else {
-        throw new Error('task can not be updated');
-      }
-    } catch (err) {
-      console.log("error", err);
-    }
-  };
-
-  const deleteTask = async (id) => {
-    try {
-      const response = await api.delete(`/tasks/${id}`);
-      
-      if(response.status === 200) {
-        console.log('success!');
-        getTasks();
-      } else {
-        throw new Error('task can not be deleted');
-      }
-    } catch (err) {
-      console.log("error", err);
-    }
-  };
-
-  // 앱이 켜졌을 때 실행. window.onload 비슷
-  useEffect(() => {
-    getTasks();
-  }, []);
-
   return (
-    <Container>
-      <Row className="add-item-row">
-        <Col xs={12} sm={10}>
-          <input
-            type="text"
-            placeholder="할일을 입력하세요"
-            className="input-box"
-            value={todoValue}
-            onChange={(event) => setTodoValue(event.target.value)}
-          />
-        </Col>
-        <Col xs={12} sm={2}>
-          <button className="button-add" onClick={addTask}>추가</button>
-        </Col>
-      </Row>
+    <Routes>
+      <Route path="/" element={<TodoPage />} />
+      <Route path="/register" element={<RegisterPage />} />
 
-      <TodoBoard todoList={todoList} deleteTask={deleteTask} updateTask={updateTask}/>
-    </Container>
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
   );
 }
 

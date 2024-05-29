fb8ef087b6206f79841abc81a400a8c00a8b11b2
Loop1397 <shwlsdn0906@gmail.com>
2024-05-30 02:19:55 +0900

    ✨ feat : 로그인 시 유저 정보 가져오도록 작성
    token을 설정하고 getUser메소드를 통해 백엔드와 통신, token인증에 성공했다면 해당 유저의 정보를 받아와 저장하는 로직 작성

--- src/App.js
+++ src/App.js
@@ -4,11 +4,34 @@ import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import TodoPage from "./pages/TodoPage";
 import RegisterPage from "./pages/RegisterPage";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PrivateRoute from "./route/PrivateRoute";
+import api from "./utils/api";
 
 function App() {
   const [user, setUser] = useState(null);
+  const getUser = async () => {
+    try {
+      const storedToken = sessionStorage.getItem("token");
+      if(storedToken) {
+        // 컴포넌트가 로드될 때 api.js에서 이미 token을 헤더에 넣는 작업이 이루어짐
+        // 그래서 다른 작업만 해주면 그만
+        
+        const response = await api.get('/user/me');
+        setUser(response.data.user);
+      }
+     
+    } catch(error) {
+      // 뭐든 실패하면 유저 데이터는 없는 것이니 null로 세팅
+      setUser(null);
+    }
+  }
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  
   return (
     <Routes>
       <Route path="/" element={
@@ -18,7 +41,7 @@ function App() {
       } />
       <Route path="/register" element={<RegisterPage />} />
 
-      <Route path="/login" element={<LoginPage />} />
+      <Route path="/login" element={<LoginPage user={user} setUser={setUser}/>} />
     </Routes>
   );
 }
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -3,14 +3,13 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import api from "../utils/api";
 
-import { Link, useNavigate } from "react-router-dom";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 
-const LoginPage = () => {
+const LoginPage = ({user, setUser}) => {
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
-  const [user, setUser] = useState(null);
 
   const navigate = useNavigate();
 
@@ -24,10 +23,11 @@ const LoginPage = () => {
         // 토큰 및 유저정보 저장
         // local storage : 브라우저를 닫아도 사라지지 않는 데이터 저장소
         // session storage : 브라우저를 닫으면 리셋되는 데이터 저장소
-        // token은 앞으로 api호출 시 header에 넣어서 전달할거임
         setUser(response.data.user);
         sessionStorage.setItem('token', response.data.token);
 
+        // token은 앞으로 api호출 시 header에 넣어서 전달할거임
+        // token을 'authorization'이란 이름을 붙여서 헤더를 통해 전달
         // Bearer는 토큰값을 보낼 때 붙이는 암묵의 룰같은 것.
         api.defaults.headers["authorization"] = "Bearer " + response.data.token;
         setError("");
@@ -39,6 +39,10 @@ const LoginPage = () => {
     } catch(error) {
       setError(error.message);
     }
+  }
+
+  if(user) {
+    return <Navigate to="/"/>
   }
 
   return (
